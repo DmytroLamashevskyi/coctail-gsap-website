1ab3e702937caa5037d224450d6302b8165a2d0d
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2025-11-17 00:53:08 +0900

    Background hot fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       "nav",
       { backgroundColor: "transparent" },
       {
-        backgroundColor: "#000050",
+        backgroundColor: "#00000050",
         backgroundFilter: "blur(10px)",
         duration: 1,
         ease: "power1.inOut",
