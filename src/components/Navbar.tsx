95b6108c9502522f2e49382ef8b2a0ee9ae9435f
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2025-11-16 19:55:23 +0900

    Added Navigation Menu

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { navLinks } from "../../constants/index";
+import { useGSAP } from "@gsap/react";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src={`${import.meta.env.BASE_URL}images/logo.png`} alt="logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
