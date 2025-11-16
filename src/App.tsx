081d33bb3bbf516af13a95b7318218a38760b424
DmytroLamashevskyi <d.lamashevskyi@gmail.com>
2025-11-16 18:58:04 +0900

    Added all libs for init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 function App() {
   return (
-    <>
-      <div>App</div>
-    </>
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-300">Hello world!</h1>
+    </div>
   );
 }
 
